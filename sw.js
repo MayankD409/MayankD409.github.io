deb9c4aa10d12d008e674296a2754296a7a827d6
MayankD409 <MayankD409@users.noreply.github.com>
2024-10-23 04:21:50 +0000

    deploy: 2b455b6341e13938fd3dd8bcd4043a5fcac6b95e

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-37fb188629939881e81c.js"
+    "url": "webpack-runtime-0ee5e57c3348758b247a.js"
   },
   {
     "url": "framework-d46d909aa0d6150f78d2.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "98f8bffc88c88c4878674d893d60f332"
+    "revision": "a5d4d494de6a5ba8d395ed7a621e8f3f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6f061b09cc63f0400406.js"
